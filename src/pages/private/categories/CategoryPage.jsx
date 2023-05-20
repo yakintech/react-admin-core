a5b2933b02d4dada996bfccb3630d9570aeaf5c7
Shahin Dostuyev <108731857+ShahinDostuyev@users.noreply.github.com>
2023-05-20 22:17:37 +0400

    edit category page

--- src/pages/private/categories/CategoryPage.jsx
+++ src/pages/private/categories/CategoryPage.jsx
@@ -1,76 +1,163 @@
-import React, { useState } from 'react'
-import { useMutation, useQuery } from 'react-query'
-import { axiosInstance } from '../../../network/axiosInstance'
-import { DataGrid } from '@mui/x-data-grid'
-import { Button, CircularProgress } from '@mui/material'
-import DetailModal from './DetailModal'
+import React, { useState } from "react";
+import { useMutation, useQuery } from "react-query";
+import { axiosInstance } from "../../../network/axiosInstance";
+import { DataGrid } from "@mui/x-data-grid";
+import { Delete as DeleteIcon,Info } from "@mui/icons-material";
+
+import DetailModal from "./DetailModal";
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import { confirmAlert } from "react-confirm-alert";
 
 function CategoryPage() {
+  const [open, setOpen] = useState(false);
+  const [name, setname] = useState("");
+  const [description, setdescription] = useState("");
+  const [confirmDialogOpen, setConfirmDialogOpen] = useState(false);
+  const [deleteParams, setDeleteParams] = useState(null);
 
-    const [open, setOpen] = useState(false);
-    const [name, setname] = useState('');
-    const [description, setdescription] = useState('')
-
-
-    const { data, error, isLoading, isSuccess, refetch } = useQuery("categoryData", () => {
-        return axiosInstance.get('categories')
-    })
-
-    //data -> number, object, array
-    const mutation = useMutation({
-        mutationFn: async (data) => {
-            let result = await axiosInstance.delete(`categories/${data}`);
-            return result;
-        },
-        onSuccess: (data) => {
-            refetch();
-        },
-        onError: (err) => {
-            console.log('Error', err);
-        }
-    })
-
-
-    const deleteCategory = (params) => {
-        mutation.mutate(params.id)
+  const { data, error, isLoading, isSuccess, refetch } = useQuery(
+    "categoryData",
+    () => {
+      return axiosInstance.get("categories");
     }
+  );
 
+  //data -> number, object, array
+  const mutation = useMutation({
+    mutationFn: async (data) => {
+      let result = await axiosInstance.delete(`categories/${data}`);
+      return result;
+    },
+    onSuccess: (data) => {
+      refetch();
+    },
+    onError: (err) => {
+      console.log("Error", err);
+    },
+  });
 
-    const showDetail = (params) => {
-        setOpen(true);
-        setname(params.row.name);
-        setdescription(params.row.description)
-    }
+  const deleteCategory = (params) => {
+    mutation.mutate(params.id);
+    setConfirmDialogOpen(false);
+    console.log("Deleted category id: ", params.id);
+  };
 
-    const columns = [
-        { field: 'id', headerName: 'Id', width: 150 },
-        { field: 'name', headerName: 'Name', width: 150 },
-        { field: 'description', headerName: 'Description', width: 150 },
-        
-        { field: "delete", headerName: "Delete", renderCell: (params) => <Button onClick={() => deleteCategory(params)} variant="outlined" color="error">Delete</Button> },
+  const handleDialogClose = () => {
+    setConfirmDialogOpen(false);
+  };
 
-        { field: "detail", headerName: "Detail", width: 250, renderCell: (params) => <Button onClick={() => showDetail(params)} variant="outlined" color="error">Show Detail</Button> }
+  const handleDeleteClick = (params) => {
+    setDeleteParams(params);
+    setConfirmDialogOpen(true);
+  };
 
-    ]
-    return (<>
-        <DetailModal open={open} setOpen={setOpen} name={name} description={description} />
+  const showDetail = (params) => {
+    setOpen(true);
+    setname(params.row.name);
+    setdescription(params.row.description);
+  };
+
+  const confirm = (params) => {
+    console.log("Confirmation started");
+
+    confirmAlert({
+      title: "Confirm to submit",
+      message: "Are you sure to do this.",
+      buttons: [
         {
-            isLoading && <CircularProgress />
-        }
+          label: "Yes",
+          onClick: () => deleteCategory(params),
+        },
         {
-            mutation.isLoading && <CircularProgress />
-        }
-        {
+          label: "No",
+          onClick: () => console.log("Delete operation is canceled"),
+        },
+      ],
+    });
+  };
 
-            isSuccess && <div style={{ height: 600, width: '100%' }}>
-                <DataGrid
-                    rows={data?.data}
-                    columns={columns}
-                />
-            </div>
-        }
+  const columns = [
+    { field: "id", headerName: "Id", width: 150 },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "description", headerName: "Description", width: 150 },
 
-    </>)
+    {
+      field: "delete",
+      headerName: "Delete",
+      renderCell: (params) => (
+        <Button
+          onClick={() => handleDeleteClick(params)}
+          variant="outlined"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          color="error"
+        >
+          <DeleteIcon />
+        </Button>
+      ),
+    },
+
+    {
+      field: "detail",
+      headerName: "Details",
+      width: 250,
+      renderCell: (params) => (
+        <Button
+          onClick={() => showDetail(params)}
+          variant="outlined"
+          color="primary"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Info />
+        </Button>
+      ),
+    },
+  ];
+
+ 
+  return (
+    <>
+      <DetailModal
+        open={open}
+        setOpen={setOpen}
+        name={name}
+        description={description}
+      />
+      {isLoading && <CircularProgress />}
+      {mutation.isLoading && <CircularProgress />}
+      {isSuccess && (
+        <div style={{ height: 600, width: "100%" }}>
+          <DataGrid rows={data?.data} columns={columns} />
+        </div>
+      )}
+      <Dialog open={confirmDialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>Confirm Delete</DialogTitle>
+        <DialogContent>
+          Are you sure you want to delete this category?
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button onClick={() => deleteCategory(deleteParams)} color="error">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
 
-export default CategoryPage
+export default CategoryPage;
